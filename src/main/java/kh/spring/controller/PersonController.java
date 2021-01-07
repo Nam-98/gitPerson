ee6ae2335b92682b9402fac5c850cea6266de459
hyelim <hohohoi914@naver.com>
2021-01-07 13:26:33 +0900

    Output added

--- src/main/java/kh/spring/controller/PersonController.java
+++ src/main/java/kh/spring/controller/PersonController.java
@@ -1,23 +1,38 @@
 package kh.spring.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
+import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
+import kh.spring.dao.PersonDAO;
+import kh.spring.service.PersonService;
 
 @Controller
 public class PersonController {
 
+	@Autowired
+	private PersonService pservice;
+	
 	@RequestMapping("/")
 	public String home() {
 		return "home";
+	}
+	
+	@RequestMapping("output.person")
+	public String toOutput(Model model) throws Exception{
+		List<PersonDAO> list = pservice.getAll();
+		model.addAttribute("list",list);
+		return "outputView";
+	}
+	
+	@ExceptionHandler
+	public String exceptionalHandler(Throwable e) {
+		e.printStackTrace();
+		return "error";
 	}
 	
 }
--- src/main/java/kh/spring/dao/PersonDAO.java
+++ src/main/java/kh/spring/dao/PersonDAO.java
@@ -1,8 +1,18 @@
 package kh.spring.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class PersonDAO {
 
+	@Autowired
+	private SqlSession db;
+	
+	public List<PersonDAO> getAll() throws Exception{
+		return db.selectList("Person.getAllList");
+	}
 }
